bad9f60d00a89b0a7f1da4abccfa0bd59dfcb427
madokainagaki <jo6gencho9@gmail.com>
2022-05-31 14:26:39 +0900

    最低限まで

---
+++ app/src/main/java/jp/techacademy/madoka/inagaki/apiapp2/ApiApplication.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.madoka.inagaki.apiapp2
+
+import android.app.Application
+import io.realm.Realm
+
+class ApiApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
