04b7382cc5549206fc2ef5c12677dea41b239541
madokainagaki <jo6gencho9@gmail.com>
2022-06-03 13:57:09 +0900

    ???????

--- app/src/main/java/jp/techacademy/madoka/inagaki/apiapp2/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/madoka/inagaki/apiapp2/WebViewActivity.kt
@@ -3,7 +3,9 @@ package jp.techacademy.madoka.inagaki.apiapp2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity: AppCompatActivity() {
@@ -12,15 +14,20 @@ class WebViewActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
         webView.loadUrl(intent.getStringExtra(KEY_URL).toString())
+
+        btnFab.setOnClickListener{
+            Log.d("test","あああ")
+        }
+
     }
 
     companion object {
         private const val KEY_URL = "key_url"
-        fun start(activity: Activity, url: String) {
+        fun start(activity: Activity, url: FavoriteShop) {
             activity.startActivity(
                 Intent(activity, WebViewActivity::class.java).putExtra(
                     KEY_URL,
-                    url
+                    FavoriteShop
                 )
             )
         }
